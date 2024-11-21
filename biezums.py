a2b942c7f44a94227f65d27c9b74f9dfdcb2f32c
FreeNFF <nauisfricsons30@gmail.com>
2024-11-21 12:20:57 +0200

    Update biezums.py

--- biezums.py
+++ biezums.py
@@ -2,6 +2,17 @@ with open("Masinmacisanas.txt",'r', encoding='utf-8') as file:#nolasa teksta dat
     data= file.read()
 print(data)
 
+print("------------------------------------------------")
+
+word_count={}
 lines = data.split()
 print(lines)
+print("------------------------------------------------")
 
+for word in lines:
+    if len(word)>=4:
+        if word in word_count:
+            word_count[word] +=1
+        else:
+            word_count = 1
+print(word_count)
