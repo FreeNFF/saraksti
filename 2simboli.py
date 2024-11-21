777c5deb4eee993dce93799e11f7256f8430bb2e
FreeNFF <nauisfricsons30@gmail.com>
2024-11-21 13:53:11 +0200

    Update 2simboli.py

--- 2simboli.py
+++ 2simboli.py
@@ -1,8 +1,10 @@
 file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
 data = file.read()
 print(data)
+print("--------------------------------------------------")
 
 print("Faila nosaukums: ",file.name)
+print("--------------------------------------------------")
 
 
 print("Simbolu skaits: ", len(data))
@@ -10,11 +12,33 @@ words = data.split()
 count_words= len(words)
 print("Vārdu skaits: ", count_words)
 
+
 file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
 lines = file.readlines()
 line_count = len(lines)
 print("Teksta līnijas kopā:", line_count)
+print("--------------------------------------------------")
+
+file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
+data = file.read(10)
+print("Pirmie desmit simboli: ",data)
+print("--------------------------------------------------")
 
 file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
 data = file.read()
+print("Pirmais simbols: ",data[0])
+print("--------------------------------------------------")
+
+file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
+data = file.read()
+
+print('Pēdējais simbols:',data[-1])
+print("--------------------------------------------------")
+
+def Pirma():
+    file= open('2teksts.txt', encoding="utf-8")#nolasa teksta datni, utf-8 - valoda
+    line = file.readlines()
+    print('Pirmā rindiņa: ', line[0])
+Pirma()
+
 
